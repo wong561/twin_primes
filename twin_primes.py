02b589ef59a6367972c5735c9a62113301d8c671
wong561 <88598378+wong561@users.noreply.github.com>
2022-08-12 21:12:31 -0400

    Add files via upload

--- twin_primes.py
+++ twin_primes.py
@@ -1,15 +1,26 @@
 def prime(n):
-    f=0
+    tp=[]
     for i in range(2,n//2+1):
         if n%i==0:
+            tp.append(i)
             f=1
+            print("here",tp)
             break
-    return f
+    return tp
 
 
 num=int(input("Enter Starting Number"))
 num1=int(input("Enter Last Number"))
 print("Twin Prime Numbers Are")
+
 for i in range(num, num1):
+    
     if prime(i)==0 and prime(i+2)==0:
+    #return
+    
+        #tp.append(i)
         print(i,i+2);
+        #print(tp)
+                 
+#tp.append(i)
+#print(tp)
